d2d3b7fd449da96be853340920ccc0464d6cc385
Madina <madina-khat@yandex.ru>
2022-10-16 13:54:38 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        for (int i = 1; i < 10; i ++) {
-            System.out.println ("Интерация цикла " +i);
+
+        public static void main(String[] args) {
+
+        for (int i = 10; i >= 1; i --) {
+            System.out.println ("Число " +i);
 
         }
 
