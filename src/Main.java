77999b1654daea1519811140d60f66833dc6d2dc
Madina <madina-khat@yandex.ru>
2022-10-16 15:38:13 +0300

    Задача 3_2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
             int salary = 29_000;
             int total = 0;
-            for (int i = 1; i < 13; i++) {
-                total += salary;
+            for (int i = 1; i <= 12; i++) {
+                total += salary + total / 100;
                 System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
-
         }
 
 }}
